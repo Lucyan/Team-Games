a119626a01ce9dcf3acbf17a039d7c7e42a623f1
Leonardo <leonardo311@gmail.com>
2013-01-13 04:15:00 -0300

    Initial Commit

---
+++ Team Games/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Team Games
+//
+//  Created by Leonardo on 13-01-13.
+//  Copyright (c) 2013 devTrash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Team Games/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Team Games
+//
+//  Created by Leonardo on 13-01-13.
+//  Copyright (c) 2013 devTrash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
